e149a4c1f8d6c31d3006b74b4b2ffddd15b7a9cb
david <davidneau49@gmail.com>
2026-03-07 22:38:00 +0100

    init

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-export const socket = io("http://localhost:5000", {
+export const socket = io(process.env.VUE_APP_API_URL, {
   autoConnect: false,
   auth: {
     token: localStorage.getItem('token')
