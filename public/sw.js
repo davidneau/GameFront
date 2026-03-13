b06c967e4b8b387659eda65cce05ea816c24e66f
david <davidneau49@gmail.com>
2026-03-13 22:19:25 +0100

    game presents

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,15 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installé')
-  self.skipWaiting()
-})
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activé')
-})
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll(self.__WB_MANIFEST); // Workbox injecte les fichiers ici
+        })
+    );
+});
 
 self.addEventListener('fetch', (event) => {
-  // Version minimale : ne fait rien de spécial pour l’instant
-})
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
