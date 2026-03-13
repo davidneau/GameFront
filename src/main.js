df8876b623ed4213db5192c471c9d1ba1b23855a
david <davidneau49@gmail.com>
2026-03-13 22:35:01 +0100

    game presents

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App)
-  .use(router)   // ⚠️ important
-  .mount('#app')
+createApp(App).use(router).mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker enregistré :', registration)
+      })
+      .catch((error) => {
+        console.error('Erreur Service Worker :', error)
+      })
+  })
+}
