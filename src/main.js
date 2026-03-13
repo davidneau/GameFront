9662bcc04a3d1f3efa6c511365bda81768c79362
david <davidneau49@gmail.com>
2026-03-13 22:26:28 +0100

    game presents

--- src/main.js
+++ src/main.js
@@ -5,15 +5,3 @@ import router from './router'
 createApp(App)
   .use(router)   // ⚠️ important
   .mount('#app')
-
-  if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        console.log('Service Worker enregistré :', registration)
-      })
-      .catch((error) => {
-        console.error('Erreur Service Worker :', error)
-      })
-  })
-}
