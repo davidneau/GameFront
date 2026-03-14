e1352cc033c8e34fd7aa8e33f554504fa9a20d8d
david <davidneau49@gmail.com>
2026-03-14 11:39:26 +0100

    fix refresh page 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import PetitBac from '../views/PetitBac.vue'
 import LoginView from '../views/Login.vue'
@@ -65,7 +65,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
