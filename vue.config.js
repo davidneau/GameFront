f4b430d63520cef93822284fc514a4c6120ca301
david <davidneau49@gmail.com>
2026-03-15 16:16:18 +0100

    snake

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: 'src/service-worker.js',
+      swSrc: './src/sw.js',
       exclude: [
         /_redirects$/,
         /_headers$/,
